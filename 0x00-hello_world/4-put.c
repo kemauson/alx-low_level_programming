df04146e3be77f3c24aa872f9a956d5e1a31d90c
kemausuor <kemausonmickline@gmail.com>
2023-03-16 07:50:06 -0700

    4-put.c

--- 0x00-hello_world/4-put.c
+++ 0x00-hello_world/4-put.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function
- * Return:0
+ * main - A program that print a line with puts function
+ * Return:0 (success)
  */
 int main(void)
 {
