cbc3fc1d7c64cc392ee0f62b739b86dd1010426f
kemausuor <kemausonmickline@gmail.com>
2023-03-17 01:06:13 -0700

    5-printf.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A programe that prints the sizee of various camputer types
+ * Return 0 (success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
