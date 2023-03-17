84fada884e2aa30b6992822758cf0ee392282edc
kemausuor <kemausonmickline@gmail.com>
2023-03-17 01:42:08 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,3 +10,10 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
+printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeofof(a));
+printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeofof(b));
+printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeofof(c));
+print("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeofof(d));
+printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeofof(f));
+return (0);
+}
